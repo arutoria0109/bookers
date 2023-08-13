d671debe1ed7d5d6b78d157c41f26b90246d8c6d
arutoria0109 <samanimaya017@gmail.com>
2023-08-13 11:39:33 +0000

    [add]show画面の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,16 +3,23 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
     @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id)
+    else
+    @books = Book.all
+    render :index
+    end
   end
 
   def show
+    @books = Book.all
+    @book = Book.find(params[:id])
   end
 
   def edit
